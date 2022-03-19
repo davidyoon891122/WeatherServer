4fec6efcd9ef96fee9170e8315448636edeaba55
davidyoon <qqpo12@naver.com>
2022-03-19 10:31:54 +0900

    add Readme, express defauls setting code

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 import express from "express"
+const app = express();
+const port = 3000;
+
+app.get("/", (req, res) => {
+    res.send("Hello Weather");
+});
+
+app.listen(port, () => {
+    console.log('Server is running',port);
+});
