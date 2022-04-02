3f94fc2df8e5bb17cf996e8a8a5a05d53c90d55e
davidyoon <qqpo12@naver.com>
2022-04-02 18:34:00 +0900

    using async aswit pattern to read apiKey from file

--- index.js
+++ index.js
@@ -1,17 +1,18 @@
 import express, { json } from "express"
 import { FileManager } from "./fileManager.js";
 import axios from "axios";
+import { fetchWeatherData } from "./networkManager.js";
 
 const app = express();
 const port = 3000;
 const fileManager = new FileManager()
 
 app.get("/", (req, res) => {
-    res.send(fileManager.getWeatherKey());
+    res.send("test");
 
 });
 
-app.get('/weather', (req, res) => {
+app.get('/weather', async (req, res) => {
     console.log(req.query.lat);
     const lat = req.query.lat
     const lon = req.query.lon
@@ -23,19 +24,11 @@ app.get('/weather', (req, res) => {
         res.send("요청한 lat값이 존재하지 않습니다.")
     } 
 
-    const apiKey = fileManager.getWeatherKey()
+    
+    const tttt = await fetchWeatherData();
 
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=37.52510&lon=126.92620&exclude=&appid=${apiKey}&lang=kr&units=metric`;
+    res.send(tttt.data)
 
-    axios
-    .get(url)
-    .then(response => {
-        res.send(response.data)
-    })
-    .catch(error => {
-        res.status(400);
-        res.send("날씨 조회 서버에 문제가 발생하여 확인중입니다.");
-    })
 })
 
 app.listen(port, () => {
--- networkManager.js
+++ networkManager.js
@@ -1,25 +1,36 @@
 import { response } from "express";
 import axios from "axios"
+import { FileManager } from "./fileManager.js";
 
-export class NetworkManager {
-    constructor(url) {
-        this.url = url;
-        this.result = ''
-    }
+// export class NetworkManager {
+//     constructor(url) {
+//         this.url = url;
+//         this.result = ''
+//     }
 
-    requestData() {
-        axios
-        .get(this.url)
-        .then(res => {
-            // console.log(res.data);
-            return res.data;
-        })
-        .catch(error => {
-            console.log(error);
-        })
-    }
+//     requestData() {
+//         axios
+//         .get(this.url)
+//         .then(res => {
+//             // console.log(res.data);
+//             return res.data;
+//         })
+//         .catch(error => {
+//             console.log(error);
+//         })
+//     }
 
-    // result() {
-    //     return this.result;
-    // }
+//     // result() {
+//     //     return this.result;
+//     // }
+// }
+
+
+
+export const fetchWeatherData = async () => {
+    const fileManager = new FileManager()
+    const apiKey = fileManager.getWeatherKey()
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=37.52510&lon=126.92620&exclude=&appid=${apiKey}&lang=kr&units=metric`;
+    const res = await axios.get(url);
+    return res
 }
