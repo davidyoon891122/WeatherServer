e94b331db7024990666b5ea6ea9f6749459be3a3
davidyoon <qqpo12@naver.com>
2022-04-03 14:37:51 +0900

    make temp path

--- fileManager.js
+++ fileManager.js
@@ -1,8 +1,12 @@
 import fs from "fs"
-
+import path from "path"
 
 export class FileManager {
     getWeatherKey() {
+        const temp_dir = path.join(process.cwd(), 'config/')
+        if (!fs.existsSync(temp_dir))
+            fs.mkdirSync(temp_dir)
+
         let data = fs.readFileSync('./config/key.json')
         let jsonData = JSON.parse(data);
         return jsonData.key
