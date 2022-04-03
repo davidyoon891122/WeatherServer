669d81027c002d2921043fed406085fc7f2c66a6
davidyoon <qqpo12@naver.com>
2022-04-03 14:15:20 +0900

    add temp weather server

--- index.js
+++ index.js
@@ -20,26 +20,19 @@ app.get('/weather', (req, res) => {
     const lang = req.query.lang
     const units = req.query.units
 
-    if (lat === '') {
-        res.status(403)
-        res.send("요청한 lat값이 존재하지 않습니다.")
-    } 
-
+    if (lat === '' || lat == null || lon === '' || lon == null) {
+        return res.status(403).send('lat 비어 있음')
+    }
     const fileManager = new FileManager()
     const apiKey = fileManager.getWeatherKey()
-    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=37.52510&lon=126.92620&exclude=&appid=${apiKey}&lang=kr&units=metric`;
+    const url = `https://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&exclude=&appid=${apiKey}&lang=kr}&units=metics}`;
     axios.get(url)
     .then(response => {
         res.send(response.data)
     })
     .catch(error => {
-        res.send(400);
-        res.send("서버 날씨 조회에 문제가 발생하였습니다.")
+        res.status(400).send('error message')
     })
-    
-
-
-
 
 })
 
