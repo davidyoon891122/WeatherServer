464bb1b1fd81dac6aca940fb63a07fc5c033ec0d
davidyoon <qqpo12@naver.com>
2022-04-03 14:32:01 +0900

    add config directory

--- fileManager.js
+++ fileManager.js
@@ -3,7 +3,7 @@ import fs from "fs"
 
 export class FileManager {
     getWeatherKey() {
-        let data = fs.readFileSync('./key.json')
+        let data = fs.readFileSync('./config/key.json')
         let jsonData = JSON.parse(data);
         return jsonData.key
 
